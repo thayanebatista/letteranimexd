96da9f5240d68eb1ba5077e0e89a148d6226b141
Thayane Batista <thayane@jera.com.br>
2024-11-25 01:30:42 -0400

    update react router

--- src/index.tsx
+++ src/index.tsx
@@ -13,13 +13,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Router>
-        <App />
-      </Router>
-    </ApolloProvider>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
