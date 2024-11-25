303868b97c5ce50b4d870b1eed3f34dec879e74c
Thayane Batista <thayane@jera.com.br>
2024-11-25 01:20:35 -0400

    react-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import Home from './pages/Home';
+
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
